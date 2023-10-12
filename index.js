f19b1a009aaf3d7ee3ee6a84fda2efc11394f89c
Hayder Abu AlHummos <greekrifleopens@gmail.com>
2023-10-12 19:10:21 +0300

    completed

--- index.js
+++ index.js
@@ -1,8 +1,45 @@
-const example = [
-  ["A", "A", "A"],
-  ["A", "A", "A"],
-  ["A", "B", "A"]];
-
 const whereIsWaldo = (matrix) => {
-  
-}
+  let commonElement = matrix[0][0];
+  let Element = null;
+  let Row, Col;
+
+  for (let row = 0; row < matrix.length; row++) {
+    for (let col = 0; col < matrix[row].length; col++) {
+      if (matrix[row][col] !== commonElement) {
+        commonRow = row;
+        commonCol = col;
+      }
+    }
+  }
+
+  for (let row = 0; row < matrix.length; row++) {
+    for (let col = 0; col < matrix[row].length; col++) {
+      if (matrix[row][col] !== commonElement) {
+        Element = matrix[row][col];
+        Row = row;
+        Col = col;
+      }
+    }
+  }
+
+  return [Row + 1, Col + 1];
+};
+
+console.log(whereIsWaldo([
+  ["O", "O", "O", "O", "O", "O", "O", "O", "O", "O"],
+  ["O", "O", "O", "O", "O", "O", "O", "O", "O", "E"],
+  ["O", "O", "O", "O", "O", "O", "O", "O", "O", "O"],
+  ["O", "O", "O", "O", "O", "O", "O", "O", "O", "O"],
+  ["O", "O", "O", "O", "O", "O", "O", "O", "O", "O"],
+  ["O", "O", "O", "O", "O", "O", "O", "O", "O", "O"],
+  ["O", "O", "O", "O", "O", "O", "O", "O", "O", "O"],
+  ["O", "O", "O", "O", "O", "O", "O", "O", "O", "O"],
+  ["O", "O", "O", "O", "O", "O", "O", "O", "O", "O"],
+  ["O", "O", "O", "O", "O", "O", "O", "O", "O", "O"],
+  ["O", "O", "O", "O", "O", "O", "O", "O", "O", "O"],
+  ["O", "O", "O", "O", "O", "O", "O", "O", "O", "O"],
+  ["O", "O", "O", "O", "O", "O", "O", "O", "O", "O"],
+  ["O", "O", "O", "O", "O", "O", "O", "O", "O", "O"],
+  ["O", "O", "O", "O", "O", "O", "O", "O", "O", "O"],
+  ["O", "O", "O", "O", "O", "O", "O", "O", "O", "O"]
+]));
